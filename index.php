3ffab35930e011a2637581c3bb2e9ddba0a37e1d
ricardo ranauro <ricbot22@gmail.com>
2022-09-13 10:46:30 -0500

    version actual

--- index.php
+++ index.php
@@ -10,18 +10,56 @@
       function table(){
         const xhttp = new XMLHttpRequest();
         xhttp.onload = function(){
-          document.getElementById("table").innerHTML = this.responseText;
+          const data = JSON.parse(this.responseText);
+          document.getElementById("table").innerHTML = data.map((element, i) => {
+            return "<tr><td>"+i+"</td><td>"+element.LATITUD+"</td><td>"+element.LONGITUD+"</td><td>"+element.FECHA+"</td></tr>";
+          }).join("\n");
+
+          if (data.length > 0) {
+            const latest = data[data.length - 1];
+            marker.setLatLng([latest.LATITUD, latest.LONGITUD]);
+            mymap.setView([latest.LATITUD, latest.LONGITUD]);
+          }
         }
-        xhttp.open("GET", "system.php");
+        xhttp.open("GET", "/webApp/system.php");
         xhttp.send();
       }
-
+x
       setInterval(function(){
         table();
-      }, 1);
+      }, 1000);
     </script>
-    <div id="table">
-
-    </div>
   </body>
+</html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page probing</title>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"
+    integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ=="
+    crossorigin=""/>
+</head>
+<body>
+    
+    <div id="myMap" style="height: 500px"></div>
+    <table style="border:1;cellpadding:10;">
+    <thead>
+      <tr>
+        <td>#</td>
+        <td>Latitud</td>
+        <td>Longitud</td>
+        <td>Timestamp</td>
+        <td>Fecha</td>
+      </tr>
+    </thead>
+    <tbody id="table">
+    </tbody>
+    </table>
+    <script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js"
+     integrity="sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ=="
+     crossorigin=""></script>
+    <script src="map.js"></script>
+</body>
 </html>
--- system.php
+++ system.php
@@ -1,23 +1,11 @@
 <?php
 $conn = mysqli_connect("datosgps.cbh1dasavvq2.us-east-1.rds.amazonaws.com", "ricardo", "ricardorobot22", "datosgps");
 $rows = mysqli_query($conn, "SELECT * FROM usuario");
+$arr = array();
+
+while ($row = mysqli_fetch_assoc($rows)) {
+  $arr[] = $row;
+}
+
+echo json_encode($arr);
 ?>
-<table border = 1 cellpadding = 10>
-  <tr>
-    <td>#</td>
-    <td>Latitud</td>
-    <td>Longitud</td>
-    <td>Timestamp</td>
-    <td>Fecha</td>
-  </tr>
-  <?php $i = 1; ?>
-  <?php foreach($rows as $row) : ?>
-    <tr>
-      <td><?php echo $i++; ?></td>
-      <td><?php echo $row["LATITUD"]; ?></td>
-      <td><?php echo $row["LONGITUD"]; ?></td>
-      <td><?php echo $row["TIMESTAMP"]; ?></td>
-      <td><?php echo $row["FECHA"]; ?></td>
-    </tr>
-  <?php endforeach; ?>
-</table>
